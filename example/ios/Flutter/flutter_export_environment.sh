50f80c974f700ebd2883737da5ec068f0c5a3570
LongYinan <lynweklm@gmail.com>
2019-11-13 12:37:16 +0800

    fix: compile error

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/cong/Documents/proj/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/cong/Documents/proj/flutter_lottie/example"
-export "FLUTTER_TARGET=/Users/cong/Documents/proj/flutter_lottie/example/lib/main.dart"
+export "FLUTTER_ROOT=/usr/local/share/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/longyinan/workspace/github/flutter_lottie/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/cong/Documents/proj/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/usr/local/share/flutter/bin/cache/artifacts/engine/ios"
